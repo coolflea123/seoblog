4caa8d4601698fe07d7beb312e48c84e93725b6f
coolflea123 <96046626+coolflea123@users.noreply.github.com>
2022-04-15 15:52:07 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+//const withCSS = require('@zeit/next-css');
+
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: 'SEOBLOG',
+        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_PRODUCTION: 'https://seoblog.com/api',
+        PRODUCTION: false,
+        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+        DOMAIN_PRODUCTION: 'https://seoblog.com',
+        FB_APP_ID: '994782494765917',
+        DISQUS_SHORTNAME: 'seoblog-jdhpdsswuh',
+        GOOGLE_CLIENT_ID: '210165718064-4g515546u6jis1vqec1599i6if2unoaj.apps.googleusercontent.com'
+    }
+};
